3687bfad3523cb0547353f3a72af551e0e07503a
Nick Siepmann <siepmann.n@gmail.com>
2024-05-30 15:54:30 +0100

    show SC pack always; changed sc pack message and achievement blurb

--- pickandmix.js
+++ pickandmix.js
@@ -3,13 +3,14 @@ window.addEventListener('load', populate);
 document.getElementById('submitButton').addEventListener("click",() => {generate()})
 var scSelect = document.getElementById('7be585f7-f51b-41b7-80ac-a37d0763e26c,b6c4b676-7d93-4c17-b26e-0ca94d4a1d85,6f93a3a0-9739-4c20-92e3-ed53bc7e54ff,fa03ff53-1f5f-4e48-9f4d-663c67ce69c8,667421ff-2156-4305-9e75-e948de51cf34,9dfb07a1-6645-4b22-898f-4c7a3f3a2d97,80ccc9e7-f12f-43d6-83dd-5b4e8a0fbf7a,467e59a3-b1bd-470c-801b-8114c1671dc8,797ec3cc-423f-4697-8503-3a8d2902fced,cabb5354-ef67-492f-8709-86f84c7b3d4f,54ce1807-46d8-4666-b148-188e592d0d87,35cfcc09-7c0d-46a3-847f-efd859ef77bc,a928186c-c186-4ae2-b79d-dd3604eb9001');
 var scSection = document.getElementById('libraryArea_SC');
-scSection.style.display = 'none';
+document.getElementById('scAlert').style.display = 'none';
 scSelect.addEventListener("click", () => {
     if (scSelect.checked == true){
-        scSection.style.display = 'none';
-        Array.from(scSection.getElementsByTagName("input")).forEach((s) => s.checked = true)
+        document.getElementById('scAlert').style.display = 'none';
+        Array.from(scSection.getElementsByTagName("input")).forEach((s) => {s.disabled = true; s.checked = true})
     } else {
-        scSection.style.display = 'block';
+        document.getElementById('scAlert').style.display = 'block';
+        Array.from(scSection.getElementsByTagName("input")).forEach((s) => {s.disabled = false; s.checked = false})
     }
 })
 
@@ -70,6 +71,7 @@ function populate(){
     buildTable(sc_applying, scLibrary[2], "SC_Pack_Applying", "Applying Sustainability","Scientific and professional knowledge are both vital to creating sustainable change - but so are interpersonal skills");
     const sc_personal = libraryAreaSC.appendChild(document.createElement("TABLE"));
     buildTable(sc_personal, scLibrary[3], "SC_Pack_Personal", "Personal Actions","Big changes start with individual action. This is your springboard to acting and living more sustainably today");
+    Array.from(scSection.getElementsByTagName("input")).forEach((s) => {s.disabled = true; s.checked = true})
 
     const mainTable = libraryArea.appendChild(document.createElement("TABLE"));
     buildTable(mainTable, mainLibrary, "main", "Our content", "Alongside our CPD offering, we have a range of content covering some more specialised topics");
@@ -78,8 +80,8 @@ function populate(){
     buildTable(placeholderTable, placeholderLibrary, "placeholder", "Coming soon", "Register your interest now and we will add these stickers to your library upon their release");
 
     const achTable = libraryArea.appendChild(document.createElement("TABLE"));
-    buildTable(achTable, achLibrary, "achievement", "Achievements", "The gamified framework that makes Stickerbook tick");
-    Array.from(achTable.getElementsByTagName("input")).forEach((s) => s.disabled = true)
+    buildTable(achTable, achLibrary, "achievement", "Achievements", "The gamified framework that makes Stickerbook tick. These stickers are automatically included in your library.");
+    Array.from(achTable.getElementsByTagName("input")).forEach((s) => {s.checked = true; s.disabled = true})
     }
 
 function generate(){
@@ -99,7 +101,7 @@ function generate(){
 function buildTable(table, library, tag, label, subtitle){
     table.id = tag;
     table.classList.add('libraryTable');
-    table.innerHTML = "<tr><td colspan=2><h2>" + label + "</h2>" + subtitle + "</td><td>Chapter</td><td><input type=checkbox id=\""+ tag + "Master\" class=\"stickerMasterSelect\" checked></td></tr>";
+    table.innerHTML = "<tr><td colspan=2><h2>" + label + "</h2>" + subtitle + "</td><td>Chapter</td><td><input type=checkbox id=\""+ tag + "Master\" class=\"stickerMasterSelect\"></td></tr>";
     var masterCheck = document.getElementById(tag + 'Master').addEventListener("change", () => {
         var boxes = Array.from(document.getElementById(tag).getElementsByClassName("stickerSelect"))
         if (document.getElementById(tag + 'Master').checked == true){
@@ -113,7 +115,7 @@ function buildTable(table, library, tag, label, subtitle){
             s.active_image_url != "" ? newRow.insertCell(-1).innerHTML = "<div class=stickerImgWrap><img class=stickerImg src=https://assets.stickerbook.tech/media/" + s.active_image_url + "></div>" : newRow.insertCell(-1) ;
             newRow.insertCell(-1).innerHTML = "<h4>" + s.sticker_name + "</h4>" + s.description;
             newRow.insertCell(-1).innerText = s.chapter;
-            newRow.insertCell(-1).innerHTML = "<input class=\"stickerSelect\" type=checkbox id=" + s.sticker_id + " checked>"
+            newRow.insertCell(-1).innerHTML = "<input class=\"stickerSelect\" type=checkbox id=" + s.sticker_id + ">"
 
             var checkbox = document.getElementById(s.sticker_id)
             checkbox.classList.add(tag);
